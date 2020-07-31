afc3e17c68c945c8743bdda73fa8bfe341f558f2
vladislavvv <vladislav713@yandex.ru>
2020-08-01 00:38:35 +0300

    rewrite Tester

--- Tester.h
+++ Tester.h
@@ -1,45 +1,108 @@
 #pragma once
 
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <functional>
 
-// Из за отсутствия фреймворка для тестирования, делаем его сами
+class TestException : public std::exception {
+public:
+	TestException(std::string message) :
+		std::exception(),
+		message(message) {}
+	
+	const char* what() const noexcept {
+		return message.c_str();
+	}
+private:
+	std::string message;
+};
+
+class NotEqualException : public TestException {
+public:
+	NotEqualException(std::string message) : TestException(message) {}
+};
+
+class EqualException : public TestException {
+public:
+	EqualException(std::string message) : TestException(message) {}
+};
+
 class Tester {
 public:
-	Tester() {
-		currentTestName = "";
-		passed = 0;
-		failed = 0;
-		step = 1;
-	}
+	Tester() : failed(0), passed(0) {}
+	void test(std::string name, std::function<void()> f);
+	void testWithException(std::string name, std::string message, std::function<void()> f);
+	template <typename T> void isEqual(T expected, T received);
+	template <typename T> void isNotEqual(T expected, T received);
+	void summary();
+private:
+	unsigned int failed;
+	unsigned int passed;
+};
 
-	void name(std::string testName) {
-		currentTestName = testName;
-		step = 1;
+void Tester::test(std::string name, std::function<void()> f) {
+	try {
+		f();
+		std::cout << "Test \"" << name << "\" passed." << std::endl;
+		passed++;
+	} catch (NotEqualException &e) {
+		std::cout << "Test \"" << name << "\" FAILED. " << e.what() << std::endl;
+		failed++;
+	} catch (EqualException &e) {
+		std::cout << "Test \"" << name << "\" FAILED. " << e.what() << std::endl;
+		failed++;
+	} catch (std::exception &e) {
+		std::cout << "Test \"" << name << "\" FAILED with unknown exception: "
+			<< e.what() << std::endl;
+		failed++;
 	}
+}
 
-	template <typename T>
-	void testEqual(T expected, T received) {
-		if (expected == received) {
-			std::cout << "TEST \"" << currentTestName << "\"" << " STEP:" << step << " passed." << std::endl;
+void Tester::testWithException(
+	std::string name,
+	std::string message,
+	std::function<void()> f
+) {
+	try {
+		f();
+		std::cout << "Test \"" << name << "\" completed."
+			<< "\n\tBUT SHOULD BE FAILED with message: <" << message << ">"
+			<< std::endl;
+		failed++;
+	} catch (std::exception &e) {
+		if (e.what() == message) {
+			std::cout << "Test \"" << name << "\" passed." << std::endl;
 			passed++;
 		} else {
-			std::cout << "TEST \"" << currentTestName << "\"" << " STEP:" << step << " FAILED !!!!!" << std::endl;
-			std::cout << "\tEXPECTED: \"" << expected << "\"" << std::endl <<
-				"\tRECEIVED: \"" << received << "\"" << std::endl;
+			std::cout << "Test \"" << name << "\" FAILED"
+				<< "\n\tWith message: <" << e.what() << ">"
+				<< "\n\tBut should with message: <" << message << ">"
+				<< std::endl;
 			failed++;
 		}
-		step++;
 	}
+}
 
-	void summary() {
-		std::cout << "Passed: " << passed << " of " << passed + failed << std::endl;
-		std::cout << "Failed: " << failed << " of " << passed + failed << std::endl;
+template <typename T>
+void Tester::isEqual(T expected, T received) {
+	if (expected != received) {
+		std::ostringstream out;
+		out << "\n\tExpected: <" << expected << ">\n\tSHOULD BE EQUAL\n\tReceived: <" << received << ">";
+		throw NotEqualException(out.str());
 	}
+}
 
-private:
-	std::string currentTestName;
-	int passed;
-	int failed;
-	int step;
-};
+template <typename T>
+void Tester::isNotEqual(T expected, T received) {
+	if (expected == received) {
+		std::ostringstream out;
+		out << "\n\tExpected: <" << expected << ">\n\tSHOULDN'T BE EQUAL to\n\treceived: <" << received << ">";
+		throw EqualException(out.str());
+	}
+}
+
+void Tester::summary() {
+	std::cout << "Passed: " << passed << " of " << passed + failed << "." << std::endl;
+	std::cout << "Failed: " << failed << " of " << passed + failed << "." << std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -2,235 +2,133 @@
 #include "json/json.h"
 #include "Tester.h"
 
-void testCreateObject(Tester &tester) {
-	tester.name("Create object");
-
-	JSON::Object object;
-	JSON::Array *array = new JSON::Array();
-
-	object.set(std::string("arr"), array);
-
-	array->push(new JSON::String("this is \" a string"));
-	array->push(new JSON::Null());
-	array->push(new JSON::Number(666));
-	array->push(new JSON::Boolean(true));
-	array->push(new JSON::Array());
-	array->push(new JSON::Object());
-
-	std::string s = object.toString();
-
-	tester.testEqual(s, std::string("{ \"arr\": [ \"this is \\\" a string\", null, 666, true, [], {} ] }"));
-}
-
-void testParseObject(Tester &tester) {
-	tester.name("Parse Object");
-
-	try {
-		auto obj = JSON::parseObject("{ \"arr\": [ \"this is \\\" a string\", null, 666, true, [], {} ] }");
-		auto arr = obj->getArray(std::string("arr"));
-
-		if (arr == nullptr) {
-			tester.testEqual("arr pointer", "is nullptr");
-			return;
-		}
-
-		auto str = arr->stringAt(0);
-		tester.testEqual(str->getValue(), std::string("this is \" a string"));
-
-		auto num = arr->numberAt(2);
-		tester.testEqual(num->getValue(), (double)666);
-
-		auto b = arr->booleanAt(3);
-		tester.testEqual(b->getValue(), true);
-
-		auto nestedArray = arr->arrayAt(4);
-		tester.testEqual(nestedArray->size(), size_t(0));
-
-		auto nestedObject = arr->objectAt(5);
-		tester.testEqual(nestedObject->getType(), JSON::Type::OBJECT);
-
-		delete obj;
-	} catch (std::exception &e) {
-		tester.testEqual(std::string(""), std::string(e.what()));
-	}
-}
-
-void testExpectedString(Tester &tester) {
-	tester.name("Expected String");
-
-	std::string message = "";
-
-	try {
-		auto obj = JSON::parse("{[}");
-		std::cout << obj->getType() << std::endl;
-	} catch (JSON::ExpectedException &exp) {
-		message = std::string(exp.what());
-	} catch (JSON::WrongLengthException &wl) {
-		message = std::string(wl.what());
-	} catch (std::exception &e) {
-		message = std::string(e.what());
-	}
-
-	tester.testEqual(message, std::string("Expected: <STRING>, but received: <[>"));
-}
-
-void testNotAllCharactersParsed(Tester &tester) {
-	tester.name("Not all characters parsed");
-
-	std::string message = "";
-
-	try {
-		auto obj = JSON::parse("{}{");
-		std::cout << obj->getType() << std::endl;
-	} catch (std::exception &e) {
-		message = std::string(e.what());
-	}
-
-	tester.testEqual(message, std::string("Not all characters parsed"));
-}
-
-void testUnexpectedEndOfJSON(Tester &tester) {
-	tester.name("Unexpected end of JSON");
-
-	std::string message = "";
-
-	try {
-		auto obj = JSON::parse("{\"key\":");
-		std::cout << obj->getType() << std::endl;
-	} catch (std::exception &e) {
-		message = std::string(e.what());
-	}
-
-	tester.testEqual(message, std::string("Unexpected end of JSON"));
-}
-
-void testExpectedTrue(Tester &tester) {
-	tester.name("Expected true");
-
-	std::string message = "";
-
-	try {
-		auto obj = JSON::parse("{\"key\":trqq}");
-		std::cout << obj->getType() << std::endl;
-	} catch (std::exception &e) {
-		message = std::string(e.what());
-	}
-
-	tester.testEqual(message, std::string("Expected: <u>, but received: <q>"));
-}
-
-void testExpectedOneOf(Tester &tester) {
-	tester.name("Expected one of");
-	std::string message = "";
-	try {
-		auto obj = JSON::parse("{\"key\":qqq}");
-		std::cout << obj->getType() << std::endl;
-	} catch (std::exception &e) {
-		message = std::string(e.what());
-	}
-
-	tester.testEqual(message, std::string("Expected: <One of char: []{}:,\" or true/false/null>, but received: <q>"));
-}
-
-void testJustAString(Tester &tester) {
-	tester.name("Just a string");
-	std::string source = "\"just a string\"";
-	try {
-		auto obj = JSON::parseString(source);
-		tester.testEqual(obj->toString(), source);
-		delete obj;
-	} catch (std::exception &e) {
-		tester.testEqual(std::string(""), std::string(e.what()));
-	}
-}
-
-void testNull(Tester &tester) {
-	tester.name("null");
-	std::string source = "null";
-	try {
-		auto obj = JSON::parse(source);
-		tester.testEqual(obj->toString(), source);
-	} catch (std::exception &e) {
-		tester.testEqual(std::string(""), std::string(e.what()));
-	}
-}
-
-void testUndefined(Tester &tester) {
-	tester.name("undefined");
-	std::string message = "";
-	try {
-		auto obj = JSON::parse("undefined");
-		std::cout << obj->getType() << std::endl;
-	} catch (std::exception &e) {
-		message = std::string(e.what());
-	}
-
-	tester.testEqual(message, std::string("Expected: <One of char: []{}:,\" or true/false/null>, but received: <u>"));
-}
-
-void testSpaceXLatestLaunch(Tester &tester) {
-	tester.name("SpaceXLatestLaunch");
-	std::string raw = "{\"fairings\":{\"reused\":null,\"recovery_attempt\":true,\"recovered\":true,\"ships\":[\"5ea6ed2e080df4000697c908\",\"5ea6ed2e080df4000697c907\"]},\"links\":{\"patch\":{\"small\":null,\"large\":null},\"reddit\":{\"campaign\":\"https://www.reddit.com/r/spacex/comments/hkbhqo/anasisii_launch_campaign_thread\",\"launch\":\"https://www.reddit.com/r/spacex/comments/hu6sci/rspacex_anasisii_official_launch_discussion/\",\"media\":\"https://www.reddit.com/r/spacex/comments/hun4pv/rspacex_anasisii_media_thread_photographer_contest/\",\"recovery\":null},\"flickr\":{\"small\":[],\"original\":[]},\"presskit\":null,\"webcast\":\"https://youtu.be/TshvZlQ7le8\",\"youtube_id\":\"TshvZlQ7le8\",\"article\":\"https://spaceflightnow.com/2020/07/20/spacex-delivers-south-koreas-first-military-satellite-into-on-target-orbit/\",\"wikipedia\":null},\"static_fire_date_utc\":\"2020-07-11T17:58:00.000Z\",\"static_fire_date_unix\":1594490280,\"tbd\":false,\"net\":false,\"window\":null,\"rocket\":\"5e9d0d95eda69973a809d1ec\",\"success\":true,\"failures\":[],\"details\":\"SpaceX will launch ANASIS-II, a South Korean geostationary military communication satellite from LC-39A, Kennedy Space Center. It will be South Korea's first dedicated military communications satellite. Falcon 9 will deliver the satellite to a geostationary transfer orbit. The booster is expected to land downrange on an ASDS.\",\"crew\":[],\"ships\":[\"5ea6ed2e080df4000697c908\",\"5ea6ed2e080df4000697c907\",\"5ea6ed2f080df4000697c90b\"],\"capsules\":[],\"payloads\":[\"5eb0e4d2b6c3bb0006eeb25b\"],\"launchpad\":\"5e9e4501f509094ba4566f84\",\"auto_update\":true,\"flight_number\":98,\"name\":\"ANASIS-II\",\"date_utc\":\"2020-07-20T21:30:00.000Z\",\"date_unix\":1595280600,\"date_local\":\"2020-07-20T17:30:00-04:00\",\"date_precision\":\"hour\",\"upcoming\":false,\"cores\":[{\"core\":\"5e9e28a7f3591817f23b2663\",\"flight\":2,\"gridfins\":true,\"legs\":true,\"reused\":true,\"landing_attempt\":true,\"landing_success\":true,\"landing_type\":\"ASDS\",\"landpad\":\"5e9e3033383ecbb9e534e7cc\"}],\"id\":\"5eb87d50ffd86e000604b394\"}";
-	try {
-		auto obj = JSON::parseObject(raw);
-
-		auto details = obj->getString(std::string("details"));
-		std::cout << "details: " << details->getValue() << std::endl;
-
-		auto date_utc = obj->getString(std::string("date_utc"));
-		std::cout << "date_utc: " << date_utc->getValue() << std::endl;
-
-		auto links = obj->getObject(std::string("links"));
-		auto reddit = links->getObject(std::string("reddit"));
-		auto launch = reddit->getString(std::string("launch"));
-		std::cout << "launch: " << launch->getValue() << std::endl;
-
-		tester.testEqual(1, 1);
-		delete obj;
-	} catch (std::exception &e) {
-		tester.testEqual(std::string(e.what()), std::string());
-	}
-}
-
-void testArraySize(Tester& tester) {
-	tester.name("Test array size");
-
-	try {
-		auto arr1 = JSON::parseArray("[1,2,3,4]");
-		tester.testEqual(arr1->size(), size_t(4));
-
-		auto arr2 = JSON::parseArray("[]");
-		tester.testEqual(arr2->size(), size_t(0));
-
-		auto arr3 = JSON::parseArray("[\"one\", null, true, 123.456, {}, {}, {}]");
-		tester.testEqual(arr3->size(), size_t(7));
-
-		delete arr1;
-		delete arr2;
-		delete arr3;
-	} catch (std::exception &e) {
-		tester.testEqual(std::string(e.what()), std::string());
-	}
-}
-
 int main() {
 	Tester tester;
 
-	testCreateObject(tester);
-	testExpectedString(tester);
-	testNotAllCharactersParsed(tester);
-	testUnexpectedEndOfJSON(tester);
-	testExpectedTrue(tester);
-	testExpectedOneOf(tester);
-	testJustAString(tester);
-	testNull(tester);
-	testUndefined(tester);
-	testParseObject(tester);
-	testSpaceXLatestLaunch(tester);
-	testArraySize(tester);
+	tester.test("Create object", [&]() {
+		JSON::Object object;
+		JSON::Array *array = new JSON::Array();
+		object.set(std::string("arr"), array);
+		array->push(new JSON::String("this is \" a string"));
+		array->push(new JSON::Null());
+		array->push(new JSON::Number(666));
+		array->push(new JSON::Boolean(true));
+		array->push(new JSON::Array());
+		array->push(new JSON::Object());
+		std::string s = object.toString();
+		tester.isEqual(s, std::string("{ \"arr\": [ \"this is \\\" a string\", null, 666, true, [], {} ] }"));
+	});
+
+	tester.test("Parse Object", [&]() {
+		auto obj = JSON::parseObject("{ \"arr\": [ \"this is \\\" a string\", null, 666, true, [], {} ] }");
+		auto arr = obj->getArray(std::string("arr"));
+		tester.isNotEqual<JSON::Array*>(arr, nullptr);
+		auto str = arr->stringAt(0);
+		tester.isEqual<std::string>(str->getValue(), "this is \" a string");
+		auto num = arr->numberAt(2);
+		tester.isEqual<double>(num->getValue(), 666);
+		auto b = arr->booleanAt(3);
+		tester.isEqual(b->getValue(), true);
+		auto nestedArray = arr->arrayAt(4);
+		tester.isEqual<size_t>(nestedArray->size(), 0);
+		auto nestedObject = arr->objectAt(5);
+		tester.isEqual(nestedObject->getType(), JSON::Type::OBJECT);
+		delete obj;
+	});
+
+	tester.testWithException(
+		"Expected String",
+		"Expected: <STRING>, but received: <[>",
+		[&]() {
+			auto obj = JSON::parse("{[}");
+			std::cout << obj->getType() << std::endl;
+		}
+	);
+
+	tester.testWithException(
+		"Exception not all characters parsed",
+		"Not all characters parsed",
+		[&]() {
+			auto obj = JSON::parse("{}{");
+			std::cout << obj->getType() << std::endl;
+		}
+	);
+
+	tester.testWithException(
+		"Exception unexpected end of JSON",
+		"Unexpected end of JSON",
+		[&]() {
+			auto obj = JSON::parse("{\"key\":");
+			std::cout << obj->getType() << std::endl;
+		}
+	);
+
+	tester.testWithException(
+		"Expected true",
+		"Expected: <u>, but received: <q>",
+		[&]() {
+			auto obj = JSON::parse("{\"key\":trqq}");
+			std::cout << obj->getType() << std::endl;
+		}
+	);
+
+	tester.testWithException(
+		"Expected one of",
+		"Expected: <One of char: []{}:,\" or true/false/null>, but received: <q>",
+		[&]() {
+			auto obj = JSON::parse("{\"key\":qqq}");
+			std::cout << obj->getType() << std::endl;
+		}
+	);
+
+	tester.test("Just a string", [&]() {
+		std::string source = "\"just a string\"";
+		auto obj = JSON::parseString(source);
+		tester.isEqual(obj->toString(), source);
+	});
+
+	tester.test("null", [&]() {
+		std::string source = "null";
+		auto obj = JSON::parse(source);
+		tester.isEqual(obj->toString(), source);
+	});
+
+	tester.testWithException(
+		"undefined",
+		"Expected: <One of char: []{}:,\" or true/false/null>, but received: <u>",
+		[&]() {
+			auto obj = JSON::parse("undefined");
+			std::cout << obj->getType() << std::endl;
+		}
+	);
+
+	tester.test("SpaceX latest launch", [&]() {
+		std::string raw = "{\"fairings\":{\"reused\":null,\"recovery_attempt\":true,\"recovered\":true,\"ships\":[\"5ea6ed2e080df4000697c908\",\"5ea6ed2e080df4000697c907\"]},\"links\":{\"patch\":{\"small\":null,\"large\":null},\"reddit\":{\"campaign\":\"https://www.reddit.com/r/spacex/comments/hkbhqo/anasisii_launch_campaign_thread\",\"launch\":\"https://www.reddit.com/r/spacex/comments/hu6sci/rspacex_anasisii_official_launch_discussion/\",\"media\":\"https://www.reddit.com/r/spacex/comments/hun4pv/rspacex_anasisii_media_thread_photographer_contest/\",\"recovery\":null},\"flickr\":{\"small\":[],\"original\":[]},\"presskit\":null,\"webcast\":\"https://youtu.be/TshvZlQ7le8\",\"youtube_id\":\"TshvZlQ7le8\",\"article\":\"https://spaceflightnow.com/2020/07/20/spacex-delivers-south-koreas-first-military-satellite-into-on-target-orbit/\",\"wikipedia\":null},\"static_fire_date_utc\":\"2020-07-11T17:58:00.000Z\",\"static_fire_date_unix\":1594490280,\"tbd\":false,\"net\":false,\"window\":null,\"rocket\":\"5e9d0d95eda69973a809d1ec\",\"success\":true,\"failures\":[],\"details\":\"SpaceX will launch ANASIS-II, a South Korean geostationary military communication satellite from LC-39A, Kennedy Space Center. It will be South Korea's first dedicated military communications satellite. Falcon 9 will deliver the satellite to a geostationary transfer orbit. The booster is expected to land downrange on an ASDS.\",\"crew\":[],\"ships\":[\"5ea6ed2e080df4000697c908\",\"5ea6ed2e080df4000697c907\",\"5ea6ed2f080df4000697c90b\"],\"capsules\":[],\"payloads\":[\"5eb0e4d2b6c3bb0006eeb25b\"],\"launchpad\":\"5e9e4501f509094ba4566f84\",\"auto_update\":true,\"flight_number\":98,\"name\":\"ANASIS-II\",\"date_utc\":\"2020-07-20T21:30:00.000Z\",\"date_unix\":1595280600,\"date_local\":\"2020-07-20T17:30:00-04:00\",\"date_precision\":\"hour\",\"upcoming\":false,\"cores\":[{\"core\":\"5e9e28a7f3591817f23b2663\",\"flight\":2,\"gridfins\":true,\"legs\":true,\"reused\":true,\"landing_attempt\":true,\"landing_success\":true,\"landing_type\":\"ASDS\",\"landpad\":\"5e9e3033383ecbb9e534e7cc\"}],\"id\":\"5eb87d50ffd86e000604b394\"}";
+		auto obj = JSON::parseObject(raw);
+		auto details = obj->getString(std::string("details"));
+		tester.isEqual<std::string>(details->getValue(), "SpaceX will launch ANASIS-II, a South Korean geostationary military communication satellite from LC-39A, Kennedy Space Center. It will be South Korea's first dedicated military communications satellite. Falcon 9 will deliver the satellite to a geostationary transfer orbit. The booster is expected to land downrange on an ASDS.");
+		auto date_utc = obj->getString(std::string("date_utc"));
+		tester.isEqual<std::string>(date_utc->getValue(), "2020-07-20T21:30:00.000Z");
+		auto links = obj->getObject(std::string("links"));
+		auto reddit = links->getObject(std::string("reddit"));
+		auto launch = reddit->getString(std::string("launch"));
+		tester.isEqual<std::string>(launch->getValue(), "https://www.reddit.com/r/spacex/comments/hu6sci/rspacex_anasisii_official_launch_discussion/");
+		delete obj;
+	});
+
+	tester.test("Array size", [&]() {
+		auto arr1 = JSON::parseArray("[1,2,3,4]");
+		tester.isEqual<size_t>(arr1->size(), 4);
+		auto arr2 = JSON::parseArray("[]");
+		tester.isEqual<size_t>(arr2->size(), 0);
+		auto arr3 = JSON::parseArray("[\"one\", null, true, 123.456, {}, {}, {}]");
+		tester.isEqual<size_t>(arr3->size(), 7);
+		delete arr1;
+		delete arr2;
+		delete arr3;
+	});
 
 	tester.summary();
 
 	return 0;
 }
-
